97f985ce42db2f88fc17903a0c4ae7dea2daf5a6
唐佳彬 <tangjiabin@3vjia.com>
2023-05-24 14:22:07 +0800

    添加glm库

--- src/glfirst/main.cpp
+++ src/glfirst/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <algorithm>
 #include <glad/glad.h>
 #include <GLFW\glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include <shader.h>
 
 float vertices[] = {
-    -0.5, -0.5, 0,
-    0.5, -0.5, 0,
-    0.5, 0.5, 0,
-    -0.5, 0.5, 0
+    -0.5, -0.5, 0, 0., 0., 1,    0.0, 0.0,
+    0.5, -0.5, 0, 0., 1, 0.,     1.0, 0.0,
+    0.5, 0.5, 0, 1, 0, 0.,       1.0, 1.0,
+    -0.5, 0.5, 0, 0, 1, 0.,      0.0, 1.0
 };
 
 GLuint indices[] = {
@@ -14,14 +19,12 @@ GLuint indices[] = {
     2, 3, 0
 };
 
-char* vertexShader = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n\n"
-"void main()\n"
-"{\n"
-"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1);\n"
-"}";
-
-char* fragShader = "#version 330 core\nout vec4 FragColor;\n\nvoid main()\n{\n    FragColor = vec4(1, 0.1, 0.3, 1);\n}";
+float texCoords[] = {
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0
+};
 
 void processInput(GLFWwindow* window)
 {
@@ -31,6 +34,8 @@ void processInput(GLFWwindow* window)
 
 int main()
 {
+    std::string strCurDif = __FILE__;
+
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -61,32 +66,60 @@ int main()
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
-    int vShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vShader, 1, &vertexShader, NULL);
-    glCompileShader(vShader);
-
-
-    int fShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fShader, 1, &fragShader, NULL);
-    glCompileShader(fShader);
-
-    GLuint shaderPro = glCreateProgram();
-    glAttachShader(shaderPro, vShader);
-    glAttachShader(shaderPro, fShader);
-    glLinkProgram(shaderPro);
-
-    glDeleteShader(vShader);
-    glDeleteShader(fShader);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
-    glEnableVertexAttribArray(0);
-
     GLuint EBO;
     glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+    strCurDif = strCurDif.substr(0, strCurDif.rfind("\\"));
+    Shader shader((strCurDif + "\\vertexShader.glsl").c_str(), (strCurDif + "\\fragShader.glsl").c_str());
+
+    std::string strAssertPath = strCurDif.substr(0, strCurDif.rfind("\\"));
+    strAssertPath = strAssertPath.substr(0, strAssertPath.rfind("\\"));
+    strAssertPath += "\\assert\\";
+    int x, y, c;
+    stbi_set_flip_vertically_on_load(true);
+    stbi_uc * data = stbi_load((strAssertPath + "container.jpg").c_str(), &x, &y, &c, 3);
+    stbi_uc* data2 = stbi_load((strAssertPath + "awesomeface.png").c_str(), &x, &y, &c, 3);
+
+
+    GLuint texture1;
+    glGenTextures(1, &texture1);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, x, y, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    GLuint texture2;
+    glGenTextures(1, &texture2);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, x, y, 0, GL_RGB, GL_UNSIGNED_BYTE, data2);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, 0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float) * 3));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float) * 6));
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+
+
+    shader.use();
+        shader.setInt("texture1", 0);
+        shader.setInt("texture2", 1);
+
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
@@ -94,8 +127,13 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderPro);
-        glBindVertexArray(VAO);
+        //shader.use();
+
+        //GLuint unifragColor = glGetUniformLocation(shaderPro, "UniFragColor");
+        //glUniform4f(unifragColor, r/255.f, g / 255.f, b / 255.f, 1.0f);
+        //glBindTexture(GL_TEXTURE_2D, texture);
+        //glBindVertexArray(VAO);
+
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwPollEvents();
